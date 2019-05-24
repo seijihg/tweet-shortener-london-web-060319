93a96a4ebf0098f24a687eaf93b46b97291be822
Le H Ngo <seiji@o2.pl>
2019-05-24 22:34:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,51 @@
 # Write your code here.
+
+def dictionary
+  dict = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "For" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  replace = dictionary()
+  tweet_array = tweet.split
+  new_w_arr = []
+
+  tweet_array.each do |w|
+    if replace.key?(w)
+      new_w_arr.push(replace[w])
+    else
+      new_w_arr.push(w)
+    end
+
+  end
+
+  tweet_returned = new_w_arr.join(" ")
+  tweet_returned
+end
+
+def bulk_tweet_shortener(tweet_arr)
+  tweet_arr.each do |i|
+    puts word_substituter(i)
+  end
+end
+
+def selective_tweet_shortener(tweet_arr)
+  tweet_arr.collect do |i|
+    if i.length > 140
+      word_substituter(i)
+    else
+      i
+    end
+  end
+end
